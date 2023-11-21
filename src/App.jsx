25a2122ddda6c4ab2a3f1ac4c1ea683719a3646f
devMeno <charlesamenlokonon@gmail.com>
2023-11-21 14:24:33 +0100

    Starting of sidebar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import LeftSidebar from './components/leftSidebar'
+import RightSidebar from './components/rightSidebar'
+
+
+function App() {
+
+    return (
+        <>
+            <LeftSidebar />
+            <RightSidebar />
+        </>
+    )
+}
+
+export default App
