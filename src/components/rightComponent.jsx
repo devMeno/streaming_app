1ffccb0ff769769d436f81c9a8d4d282f299ad4b
devMeno <charlesamenlokonon@gmail.com>
2023-12-02 23:41:59 +0100

    Continuous

--- src/components/rightComponent.jsx
+++ src/components/rightComponent.jsx
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 export default function RightComponent() {
      return (
           <>
-               <div className="fixed right-0 top-0 w-1/5 bg-gray-100  overflow-auto">
+               <div className="fixed right-0 top-0 w-1/5 bg-gray-100 py-4">
                     <div className="flex items-center mx-4">
                          <UserCircleIcon className="h-16 w-16 text-gray-500" />
                          <div className="">
@@ -13,7 +13,7 @@ export default function RightComponent() {
                          </div>
                     </div>
                     <div className="flex bg-red-300 mx-4">
-                         <span>Fans also like</span>
+                         <span className="text-lg font-bold">Fans also like</span>
                          <span className="fixed right-0 mr-4">See all</span>
                     </div>
                     <Card style={{ width: '17rem', padding: '0 16px 0 16px' }}>
@@ -24,40 +24,8 @@ export default function RightComponent() {
                          </Card.Body>
                     </Card>
                     <div className="flex bg-red-300 mx-4">
-                         <span>Fans also like</span>
+                         <span className="text-lg font-semibold">Fans also like</span>
                          <span className="fixed right-0 mr-4">See all</span>
-                    </div>
-                    <div className="flex items-center my-2 mx-4">
-                         <img src="https://fakeimg.pl/600x400" alt="" className="w-12 h-12 rounded-lg" />
-                         <div className="ml-4">
-                              <span>Title</span><br />
-                              <span>Autor</span>
-                         </div>
-                         <div className="fixed right-0 mr-4"><p>2min ago</p></div>
-                    </div>
-                    <div className="flex items-center my-2 mx-4">
-                         <img src="https://fakeimg.pl/600x400" alt="" className="w-12 h-12 rounded-lg" />
-                         <div className="ml-4">
-                              <span>Title</span><br />
-                              <span>Autor</span>
-                         </div>
-                         <div className="fixed right-0 mr-4"><p>2min ago</p></div>
-                    </div>
-                    <div className="flex items-center my-2 mx-4">
-                         <img src="https://fakeimg.pl/600x400" alt="" className="w-12 h-12 rounded-lg" />
-                         <div className="ml-4">
-                              <span>Title</span><br />
-                              <span>Autor</span>
-                         </div>
-                         <div className="fixed right-0 mr-4"><p>2min ago</p></div>
-                    </div>
-                    <div className="flex items-center my-2 mx-4">
-                         <img src="https://fakeimg.pl/600x400" alt="" className="w-12 h-12 rounded-lg" />
-                         <div className="ml-4">
-                              <span>Title</span><br />
-                              <span>Autor</span>
-                         </div>
-                         <div className="fixed right-0 mr-4"><p>2min ago</p></div>
                     </div>
                     <div className="flex items-center my-2 mx-4">
                          <img src="https://fakeimg.pl/600x400" alt="" className="w-12 h-12 rounded-lg" />
